5f56bb0b524960bff48d4dcd08331efbe02c4ffe
Luke Chambers <lukenatchambers@gmail.com>
2018-05-31 10:28:35 -0600

    Wire together JS files.

---
+++ public/assets/main.js
@@ -0,0 +1,9 @@
+$('.nav-login-link').on('click', function() {
+  console.log('oh hello there')
+});
+
+$('.nav-about-link').on('click', function() {
+  console.log('about')
+});
+
+console.log('hello')
