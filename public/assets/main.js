b0ea97130ac1da0e4d2ed9eec0809a613f84e6e5
Luke Chambers <lukenatchambers@gmail.com>
2018-05-31 13:39:12 -0600

    Pseudocode the jquery functionality

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,14 +1,17 @@
 $( document ).ready(function() {
   console.log( "ready!" );
-  $('.nav-login-link').click(() => {
-    console.log('oh hello there')
+  $('.nav-login-link').click( () => {
+    // Pull down the header and display the login form
   });
-
-  $('.nav-about-link').click(() => {
-    console.log('about')
+  $('.nav-about-link').click( () => {
+    // Pull down the header and display the about us information
   });
-
-  $('.logo').on('click', () => {
-    console.log('This is the logo')
+  $('.logo').click( () => {
+    // Reset CSS back to the original home page.
+  });
+  $('.content')
+    // Load's content partials
+  $('.triangle-up').click( () => {
+    // Pull up the triangle to show information on plants
   });
 });
