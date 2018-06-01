2682fab1d0463e7bfbf13090b4e258e64547fe70
chunktooth <om.saetehn@gmail.com>
2018-05-31 18:52:51 -0600

    slideDown too hard

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,17 +1,24 @@
 $( document ).ready(function() {
   console.log( "Execute order 66" );
-  $('.nav-login-link').click( () => {
-    // Pull down the header and display the login form
-  });
-  $('.nav-about-link').click( () => {
-    // Pull down the header and display the about us information
-  });
+
+  $('.nav-login-link').on('click', enterNinja);
+  $('.nav-about-link').on('click', enterNinja);
+
   $('.logo').click( () => {
     // Reset CSS back to the original home page.
   });
+
   $('.content')
     // Load's content partials
+
   $('.triangle-up').click( () => {
     // Pull up the triangle to show information on plants
   });
 });
+
+function enterNinja() {
+  $('header').slideDown('slow', function() {
+    $('header').addClass('fill')
+    $('.triangle-up').toggleClass('z-backward')
+  })
+};
