1e5a434a2b43912fc3171f1495904d5008df1c97
Luke Chambers <lukenatchambers@gmail.com>
2018-06-02 20:02:53 -0600

    Get the page to transition

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,12 +1,9 @@
 $( document ).ready(function() {
   console.log( "Execute order 66" );
 
-  $('.nav-login-link').on('click', enterNinja);
-  $('.nav-about-link').on('click', enterNinja);
-
-  $('.logo').click( () => {
-    // Reset CSS back to the original home page.
-  });
+  $('.nav-login-link').on('click', enterLoginNinja);
+  $('.nav-about-link').on('click', enterAboutNinja);
+  $('.logo').on('click', exitNinja)
 
   $('.content')
     // Load's content partials
@@ -16,9 +13,47 @@ $( document ).ready(function() {
   });
 });
 
-function enterNinja() {
-  $('header').slideDown('slow', function() {
-    $('header').addClass('fill')
-    $('.triangle-up').toggleClass('z-backward')
-  })
+function enterLoginNinja() {
+  if ( $('.header:not(.fill)') ) {
+    $('.header').addClass('fill')
+    $('.logo').empty()
+    $('.logo').append('<img src="https://i.imgur.com/Yo4kvcL.png" />')
+    $('.content').addClass('hide')
+  };
+  if ( $('.about').hasClass('show') ) {
+    $('.about').removeClass('show')
+  };
+  if ( $('.login-form:not(.show)') ) {
+    $('.login-form').addClass('show')
+  };
 };
+
+function enterAboutNinja() {
+  if ( $('.header:not(.fill)') ) {
+    $('.header').addClass('fill')
+    $('.logo').empty()
+    $('.logo').append('<img src="https://i.imgur.com/Yo4kvcL.png" />')
+    $('.content').addClass('hide')
+  };
+  if ( $('.login-form').hasClass('show') ) {
+    $('.login-form').removeClass('show')
+  };
+  if ( $('.about:not(.show)') ) {
+    $('.about').addClass('show')
+  };
+};
+
+function exitNinja() {
+  if ( $('.header').hasClass('fill') ) {
+    $('.header').removeClass('fill')
+  };
+  if ( $('.about').hasClass('show') ) {
+    $('.about').removeClass('show')
+  };
+  if ( $('.login-form').hasClass('show') ) {
+    $('.login-form').removeClass('show')
+  };
+  $('.logo').empty()
+  $('.logo').append('<img src="https://i.imgur.com/8ex0RmM.png" />')
+  $('.content').removeClass('hide')
+}
