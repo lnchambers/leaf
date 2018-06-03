8f02fe34cedeb9021fad9d0600bae5201441f375
Luke Chambers <lukenatchambers@gmail.com>
2018-06-02 22:27:47 -0600

    Get transitions working

--- public/assets/main.js
+++ public/assets/main.js
@@ -3,64 +3,64 @@ $( document ).ready(function() {
 
   $('.nav-login-link').on('click', enterLoginNinja);
   $('.nav-about-link').on('click', enterAboutNinja);
-  $('.logo').on('click', exitNinja)
-
-  $('.content')
-    // Load's content partials
-
+  $('.logo').on('click', exitNinja);
+  $('.content').on('click', takePictureNinja);
+  $('.submit-button').on('click', loginNinja);
   $('.triangle-up').click( () => {
     // Pull up the triangle to show information on plants
   });
 });
 
 function enterLoginNinja() {
-  if ( $('.header:not(.fill)') ) {
-    $('.header').addClass('fill')
-    $('.logo').empty()
-    $('.logo').append('<img src="https://i.imgur.com/Yo4kvcL.png" />')
-    $('.logo').addClass('logo-about-position')
-    $('.content').addClass('hide')
-    $('.main').addClass('main-position')
-  };
-  if ( $('.about').hasClass('show') ) {
+  if ( $('.header').hasClass('fill') ) {
+    $('.logo').removeClass('logo-about-position')
+    $('.logo').addClass('logo-login-position')
+    $('.login-form').addClass('show login-position')
     $('.about').removeClass('show')
-  };
-  $('.logo').removeClass('logo-about-position')
-  $('.logo').addClass('logo-login-position')
-  $('.login-form').addClass('show login-position')
+  } else {
+    $('.content').animate({opacity: 0}, 500)
+    $('.header').animate({height: "69vh"}, 750, () => {
+      $('.header').addClass('fill')
+      $('.logo').empty()
+      $('.logo').append('<img src="https://i.imgur.com/Yo4kvcL.png" />')
+      $('.main').addClass('main-position')
+      $('.about').removeClass('show')
+      $('.logo').addClass('logo-login-position')
+      $('.login-form').addClass('show login-position')
+    })
+  }
 };
 
 function enterAboutNinja() {
-  if ( $('.header:not(.fill)') ) {
-    $('.header').addClass('fill')
-    $('.logo').empty()
-    $('.logo').append('<img src="https://i.imgur.com/Yo4kvcL.png" />')
-    $('.logo').addClass('logo-about-position')
-    $('.content').addClass('hide')
-    $('.main').addClass('main-position')
-  };
-  if ( $('.login-form').hasClass('show') ) {
+  if ( $('.header').hasClass('fill') ) {
     $('.login-form').removeClass('show')
+    $('.logo').removeClass('logo-login-position')
+    $('.logo').addClass('logo-about-position')
+    $('.about').addClass('show about-position')
+  } else {
+    $('.content').animate({opacity: 0}, 500)
+    $('.header').animate({height: "69vh"}, 750, () =>{
+      $('.header').addClass('fill')
+      $('.logo').empty()
+      $('.logo').append('<img src="https://i.imgur.com/Yo4kvcL.png" />')
+      $('.main').addClass('main-position')
+      $('.login-form').removeClass('show')
+      $('.logo').addClass('logo-about-position')
+      $('.about').addClass('show about-position')
+    });
   };
-  $('.logo').removeClass('logo-login-position')
-  $('.logo').addClass('logo-about-position')
-  $('.about').addClass('show about-position')
 };
 
 function exitNinja() {
-  if ( $('.header').hasClass('fill') ) {
+  $('.header').animate({height: "14vh"}, 750, () => {
+    $('.content').animate({opacity: 1}, 300)
     $('.header').removeClass('fill')
-  };
-  if ( $('.about').hasClass('show') ) {
+    $('.logo').empty()
+    $('.logo').append('<img src="https://i.imgur.com/8ex0RmM.png" />')
+    $('.main').removeClass('main-position')
+    $('.logo').removeClass('logo-about-position')
+    $('.logo').removeClass('logo-login-position')
     $('.about').removeClass('show')
-  };
-  if ( $('.login-form').hasClass('show') ) {
     $('.login-form').removeClass('show')
-  };
-  $('.logo').empty()
-  $('.logo').append('<img src="https://i.imgur.com/8ex0RmM.png" />')
-  $('.content').removeClass('hide')
-  $('.logo').removeClass('logo-about-position')
-  $('.logo').removeClass('logo-login-position')
-  $('.main').removeClass('main-position')
+  })
 }
